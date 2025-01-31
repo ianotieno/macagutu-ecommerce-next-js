88bd6faaec9f3a7e6df00891338ae8bab7e1b586
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2025-01-31 15:14:35 +0300

    review

--- src/app/(root)/product/[slug]/page.tsx
+++ src/app/(root)/product/[slug]/page.tsx
@@ -8,12 +8,15 @@ import ProductPrice from '@/components/shared/product/product-price'
 import ProductGallery from '@/components/shared/product/product-gallery'
 import { Separator } from '@/components/ui/separator'
 import { getProductBySlug, getRelatedProductsByCategory } from '@/lib/actions/product.action'
-import Rating from '@/components/shared/product/rating'
+
 import ProductSlider from '@/components/shared/product/product-slider'
 import BrowsingHistoryList from '@/components/shared/browsing-history-list'
 import AddToBrowsingHistory from '@/components/shared/product/add-to-browsing-history'
 import AddToCart from '@/components/shared/product/add-to-cart'
 import { generateId, round2 } from '@/lib/utils'
+import RatingSummary from '@/components/shared/product/rating-summary'
+import ReviewList from './review-list'
+import { auth } from '../../../../../auth'
 
 export async function generateMetadata(props: {
   params: Promise<{ slug: string }>
@@ -52,7 +55,7 @@ export default async function ProductDetails(props: {
     page: Number(page || '1'),
   })
 
-
+  const session = await auth()
   return (
     <div>
       <AddToBrowsingHistory id={product._id} category={product.category} />
@@ -69,8 +72,12 @@ export default async function ProductDetails(props: {
               </p>
               <h1 className='font-bold text-lg lg:text-xl'>{product.name}</h1>
                 <div className='flexitem-center gap-2'>
-                    <Rating rating={product.avgRating} />
-                    <span>{product.numReviews} rating</span>
+                <RatingSummary
+                avgRating={product.avgRating}
+                numReviews={product.numReviews}
+                asPopover
+                ratingDistribution={product.ratingDistribution}
+              />
                     </div>
               <Separator />
               <div className='flex flex-col gap-3 sm:flex-row sm:items-center'>
@@ -146,6 +153,12 @@ export default async function ProductDetails(props: {
             
           </div>
         </div>
+      </section>
+      <section className='mt-10'>
+        <h2 className='h2-bold mb-2' id='reviews'>
+         Customer Reviews
+        </h2>
+        <ReviewList product={product} userId={session?.user.id} />
       </section>
       <section className='mt-10'>
         <ProductSlider
--- src/lib/validator.ts
+++ src/lib/validator.ts
@@ -13,6 +13,20 @@ const Price = (field: string) =>
     .string()
     .regex(/^[0-9a-fA-F]{24}$/, { message: 'Invalid MongoDB ID' })
 
+
+    export const ReviewInputSchema = z.object({
+      product: MongoId,
+      user: MongoId,
+      isVerifiedPurchase: z.boolean(),
+      title: z.string().min(1, 'Title is required'),
+      comment: z.string().min(1, 'Comment is required'),
+      rating: z.coerce
+        .number()
+        .int()
+        .min(1, 'Rating must be at least 1')
+        .max(5, 'Rating must be at most 5'),
+    })
+
 export const ProductInputSchema = z.object({
   name: z.string().min(3, 'Name must be at least 3 characters'),
   slug: z.string().min(3, 'Slug must be at least 3 characters'),
@@ -41,7 +55,7 @@ export const ProductInputSchema = z.object({
   ratingDistribution: z
     .array(z.object({ rating: z.number(), count: z.number() }))
     .max(5),
-  reviews: z.array(z.string()).default([]),
+  reviews: z.array(ReviewInputSchema).default([]),
   numSales: z.coerce
     .number()
     .int()
@@ -168,3 +182,4 @@ export const OrderInputSchema = z.object({
   isPaid: z.boolean().default(false),
   paidAt: z.date().optional(),
 })
+
